9c7c7392261d7c2e069740025f90d83164dc9553
rajinda1050 <rajinda.dissanayake@gmail.com>
2020-06-07 19:56:55 +0200

    Initial commit

---
+++ src/test/java/com/rad/rdconfig/RDConfigRootTests.java
@@ -0,0 +1,13 @@
+package com.rad.rdconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RDConfigRootTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
